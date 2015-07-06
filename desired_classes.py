66932b771a4f3e25b148217cc141bb10237ac6c9
Kyle Rosales <kyle.rosales97@gmail.com>
2015-07-06 13:43:04 +0800

    added add() and print_classes() methods, and fixed __repr__()

--- desired_classes.py
+++ desired_classes.py
@@ -4,11 +4,22 @@ from utils import *
 class DesiredClasses:
     """docstring for DesiredClasses"""
     def __init__(self):
-        self.enlisted_classes = [Course("Class", 'XYZ', 0.0, 0, 0)]
+        self.enlisted_classes = [Course("Class", 'XYZ', 0, 0, 0.0)]
 
     def __repr__(self):
         s = ' '.join([center(x) for x in ['Rank', 'Class [Slots/Demand]', 'Credits']])
         s += '\n'
-        for x in xrange(0,len(self.enlisted_classes)):
+        first = True
+        for x in xrange(1,len(self.enlisted_classes)):
+            if not first:
+                s += '\n'
+            else:
+                first = False
             s += center(x) + str(self.enlisted_classes[x])
         return s
+
+    def add(self, *args):
+        self.enlisted_classes.append(Course(*args))
+
+    def print_classes(self):
+        print self
