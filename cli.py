02ae074d0358e83bcbc78f482a33d291bd3007da
Kyle Rosales <kyle.rosales97@gmail.com>
2015-07-06 15:33:34 +0800

    added auto load and auto save

--- cli.py
+++ cli.py
@@ -5,10 +5,19 @@ class CLI:
     desired_classes = DesiredClasses()
 
     def start(self):
+        try: # to catch me fighting dirty
+            f = open('save.csv')
+            f.close()
+            self.load()
+        except:
+            pass
+            
+
         while True:
             line_input = raw_input('> ').strip()
 
             if line_input == 'exit':
+                self.save()
                 return
 
             self.execute(line_input)
@@ -36,12 +45,12 @@ class CLI:
     def print_classes(self, args):
         self.desired_classes.print_classes()
 
-    def save(self, args):
+    def save(self, args = []):
         if not args:
             args = ['save']
         self.desired_classes.save(args[0])
 
-    def load(self, args):
+    def load(self, args = []):
         if not args:
             args = ['save']
         self.desired_classes.load(args[0])
