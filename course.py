770b2c358123561c92ea8782b55b6612239d6ed5
Kyle Rosales <kyle.rosales97@gmail.com>
2015-07-06 13:41:53 +0800

    added typecasting to variable initialization and added support for variable length arguments

--- course.py
+++ course.py
@@ -2,12 +2,12 @@ from utils import *
 
 class Course():
     """TODO: docstring for Course"""
-    def __init__(self, name, section, credits, slots, demand):
-        self.name = name
-        self.section = section
-        self.credits = credits
-        self.slots = slots
-        self.demand = demand
+    def __init__(self, name, section, slots, demand, credits = 3.0, *args):
+        self.name = str(name)
+        self.section = str(section)
+        self.credits = float(credits)
+        self.slots = int(slots)
+        self.demand = int(demand)
 
     def __repr__(self):
         s = '{0} {1} [{2}/{3}]'.format(str(self.name), str(self.section), str(self.slots), str(self.demand))
