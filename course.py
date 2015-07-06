084414e5b055ba8a9312e2d7c3c51251d64e78e7
Kyle Rosales <kyle.rosales97@gmail.com>
2015-07-06 12:37:27 +0800

    changed representation (__repr__) of class

--- course.py
+++ course.py
@@ -1,3 +1,5 @@
+from utils import *
+
 class Course():
     """TODO: docstring for Course"""
     def __init__(self, name, section, credits, slots, demand):
@@ -8,6 +10,5 @@ class Course():
         self.demand = demand
 
     def __repr__(self):
-        s = 'Class [Slots/Demand]\tCredits\n'
-        s += str(self.name) + ' ' + str(self.section) + ' [' + str(self.slots) + '/' + str(self.demand) + ']\t' + str(self.credits) + '.0'
-        return s
+        s = '{0} {1} [{2}/{3}]'.format(str(self.name), str(self.section), str(self.slots), str(self.demand))
+        return center(s) + ' ' + center(self.credits)
